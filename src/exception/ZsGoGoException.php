404bf43fc056b9b0887fe5286b73539bd9bcd47a
张帅 <5346812+zhangshuaiii@user.noreply.gitee.com>
2022-11-16 10:14:42 +0800

    添加异常类

--- src/exception/ZsGoGoException.php
+++ src/exception/ZsGoGoException.php
@@ -3,11 +3,7 @@ declare(strict_types=1);
 
 namespace ZsGoGo\exception;
 
-/**
- * 包的基础异常，方便外部统一拦截
- * Class ChengYiException
- * @package ChengYi\exception
- */
+
 class ZsGoGoException extends \Exception
 {
 }
